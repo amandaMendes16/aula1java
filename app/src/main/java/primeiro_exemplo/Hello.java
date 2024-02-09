bbe7e8b6e0d63f21116b5d9b3ffa1812e29ef7d6
amandaMendes16 Mendes <amasantosmendes@gmail.com>
2024-02-09 15:13:10 +0000

    Olá Mundo

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -2,6 +2,10 @@ package primeiro_exemplo;
 
 public class Hello{
     public static void main (String[] args){
+        int cont = 17;
+        String nome = "Amanda Santos";
         System.out.println("Olá Mundo!");
+
+        System.out.println(cont + ":" + nome);
     }
 }
